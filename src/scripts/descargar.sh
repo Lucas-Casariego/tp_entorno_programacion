dab15956373b1140266a7ce783c32052271102e9
lucas <lucascasariego435@gmail.com>
2024-07-14 04:30:17 -0300

    descargar imagenes y renombrarlas

--- src/scripts/descargar.sh
+++ src/scripts/descargar.sh
@@ -5,10 +5,23 @@
 # El nombre del archivo deberá ser su suma de verificación y debe terminar en .jpg
 # Asegúrese de devolver un valor de salida acorde a la situación.
 
+CLASE="$1"
+
 ./internet.sh
 
-if [ $? -eq 0 ]; then   
-    echo "Hay internet, continuando con la descarga"
-    # implementación de descarga
-else
+if [ $? -ne 0 ]; then   
     exit 1
+fi    
+
+LINK_IMAGENES="https://picsum.photos/512"
+TEMP_NAME="temp_name.jpg"
+curl -s -L "$LINK_IMAGENES" -o "$TEMP_NAME"
+if [ $? -ne 0 ]; then
+    echo "La descarga de la imagen ha fallado"
+    exit 1
+fi
+
+CHECKSUM=$(sha256sum "$TEMP_NAME" | cut -d " " -f 1)
+mv "$TEMP_NAME" "${CHECKSUM}.jpg"
+
+exit 0
