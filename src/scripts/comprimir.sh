bbaceda12b3d57b9b082762f2fbb8d6f47399420
lucas <lucascasariego435@gmail.com>
2024-07-20 18:00:06 -0300

    eliminar comentarios y renombrar variables

--- src/scripts/comprimir.sh
+++ src/scripts/comprimir.sh
@@ -3,25 +3,20 @@
 # Este script comprime todo el contenido de la carpeta actual y además, genera
 # una suma de verificación del archivo resultante en un archivo separado.
 
-# Nombre del archivo comprimido
-OUTPUT_FILE="contenido_comprimido.tar.gz"
-# Nombre del archivo de suma de verificación
+IMAGENES_COMPRIMIDAS="imagenes.tar.gz"
+
 CHECKSUM_FILE="checksum.txt"
 
-# Comprimir el contenido de la carpeta actual
-tar -czf "$OUTPUT_FILE" ./*
+tar -czf "$IMAGENES_COMPRIMIDAS" ./*
 
-# Verificar si la compresión fue exitosa
 if [ $? -eq 0 ]; then
-  echo "Compresión completada exitosamente: $OUTPUT_FILE"
+  echo "Compresión completada exitosamente: $IMAGENES_COMPRIMIDAS"
 else
   echo "Error durante la compresión." && exit 1
 fi
 
-# Generar la suma de verificación
-sha256sum "$OUTPUT_FILE" > "$CHECKSUM_FILE"
+sha256sum "$IMAGENES_COMPRIMIDAS" > "$CHECKSUM_FILE"
 
-# Verificar si la generación de la suma de verificación fue exitosa
 if [ $? -eq 0 ]; then
   echo "Suma de verificación generada exitosamente: $CHECKSUM_FILE"
 else
