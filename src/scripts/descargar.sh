445763a335cb20893fc73ef7219b3192efb3f1fb
lucas <lucascasariego435@gmail.com>
2024-07-20 19:31:08 -0300

    eliminar cambios de archivos no correspondientes a la rama

--- src/scripts/descargar.sh
+++ src/scripts/descargar.sh
@@ -5,23 +5,4 @@
 # El nombre del archivo deberá ser su suma de verificación y debe terminar en .jpg
 # Asegúrese de devolver un valor de salida acorde a la situación.
 
-CLASE="$1"
-
-./internet.sh
-
-if [ $? -ne 0 ]; then   
-    exit 1
-fi    
-
-LINK_IMAGENES="https://picsum.photos/512"
-TEMP_NAME="temp_name.jpg"
-curl -s -L "$LINK_IMAGENES" -o "$TEMP_NAME"
-if [ $? -ne 0 ]; then
-    echo "La descarga de la imagen ha fallado"
-    exit 1
-fi
-
-CHECKSUM=$(sha256sum "$TEMP_NAME" | cut -d " " -f 1)
-mv "$TEMP_NAME" "${CHECKSUM}.jpg"
-
-exit 0
+echo Descarga de imágenes de internet no implementada. && exit 1
